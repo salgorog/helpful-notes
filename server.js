ca09ab989a7aa9dc6f6f434e2b101d63489b164c
Aztec <salgorog@hotmail.com>
2020-10-11 23:18:18 -0600

    set up express server

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+fs.readFile("./db/db.json","json",function(data) {
+    var notes = data;
+});
+
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});   
