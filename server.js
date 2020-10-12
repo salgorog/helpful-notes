5fe6f17c29bfe946fdf85cb74fbc5d67b0b11443
Aztec <salgorog@hotmail.com>
2020-10-11 22:33:40 -0600

    Add Heorku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require("fs");
 const path = require('path');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.listen(3001, () => {
