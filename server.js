5e729b31559459d8a32083cf87988d16499a35eb
Aztec <salgorog@hotmail.com>
2020-11-04 16:28:43 -0700

    finished app, working with delete buttons

--- server.js
+++ server.js
@@ -1,24 +1,60 @@
-// Require Dependencies
 const express = require('express');
-const fs = require("fs");
 const path = require('path');
+const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3002;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
-
-fs.readFile("./db/db.json","json",function(data) {
-    var notes = data;
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
+app.get('api/notes/:id', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes[req.params.id]);
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});   
+app.post('/api/notes', (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let noteId = (notes.length + 1).toString();
+  newNote.id = noteId;
+  notes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
+  res.json(notes);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  
+  savedNotes = notes.filter(currNote => {
+      return currNote.id != noteID;
+  })
+  
+  for (currNote of savedNotes) {
+      currNote.id = newID.toString();
+      newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes, null, 2));
+  res.json(savedNotes);
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
